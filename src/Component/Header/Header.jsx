f738bb5cd8763aaaa8699e97ddd4e9834f3b78d6
IamAditya-Dubey <sursaritatechknow@gmail.com>
2024-09-06 11:28:50 +0530

    fix search form in mobile view

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -9,13 +9,15 @@ import { CiSearch } from "react-icons/ci";
 import { IoMdMic } from "react-icons/io";
 import { RxHamburgerMenu } from "react-icons/rx";
 import css from "./Header.module.css"
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { SiteContext } from "../../Store/store";
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 import micOn from "../../assets/mic-on.mp3"
 import micOff from "../../assets/mic-off.mp3"
 
 function Header() {
+
+  const[isSearchMob, setSearchMob] = useState(false);
 
   const {
     transcript,
@@ -70,9 +72,9 @@ function Header() {
   }
 
   return (
-    <nav id={`${css.navigation}`}>
-      <div className={`${css.logoArea}`}>
-        <div className={css.menuIcon}><RxHamburgerMenu style={{fontSize: "25px"}} onClick={() => {
+    <nav id={css.navigation} className={isSearchMob ? "!p-0" : ""}>
+      <div className={`${css.logoArea} ${isSearchMob ? "gap-[5px] md:gap-[25px]" : "gap-[25px]"}`}>
+        <div className={`${css.menuIcon} ${isSearchMob ? "md:block hidden" : ""}`}><RxHamburgerMenu style={{fontSize: "25px"}} onClick={() => {
           if(isSidebarOpen) {
             setSidebarOpen(false)
           } else if(!isSidebarOpen){
@@ -84,24 +86,25 @@ function Header() {
           }
 
         }}/></div>
-        <Link to="/"><img src={Logo} alt="logo" /></Link>
+        <Link to="/"><img src={Logo} alt="logo" className={isSearchMob && 'hidden md:block'}/></Link>
+        <div onClick={() => {setSearchMob(false)}} className={isSearchMob ? "self-start max-w-[10px] cursor-pointer pr-[20px] text-[30px] md:hidden" : "hidden"}>&#8672;</div>
       </div>
-      <div className={`${css.middleArea}`}>
+      <div className={`${css.middleArea} ${isSearchMob && css.searchClick}`}>
       <form className={`${css.searchBar}`} onSubmit={(e) => searchSubmit(e)}>
         <input type="text" placeholder="Search" ref={searchValue}/>
-        <button className="md:py-[7px] md:pr-4 md:pl-6 md:bg-gray-50 md:border-l-[1px] md:border-gray-200 md:rounded-r-3xl">
+        <button onClick={() => setSearchMob(true)} className="md:py-[7px] md:pr-4 md:pl-6 md:bg-gray-50 md:border-l-[1px] md:border-gray-200 md:rounded-r-3xl">
           <CiSearch className={css.searchIcon} />
         </button>
         </form>
         <div onClick={micListen}>
-        <IoMdMic className={css.micIcon} style={{color: listening ? "red" : ""}} />
+        <IoMdMic className={`${css.micIcon} ${isSearchMob ? css.searchClickMob : ""}`} style={{color: listening ? "red" : ""}} />
       </div>
       </div>
       <div className={`${css.rightArea}`}>
         <div className={css.extraIcons}><img src={Upload} alt="upload" /></div>
         <div className={css.extraIcons}><img src={Explore} alt="explore" /></div>
         <div className={css.extraIcons}><img src={Notification} alt="notification" /></div>
-        <div><img className={css.avatarImg} style={{width: "35px", height:"35px"}} src={User1} alt="user" /></div>
+        <div><img className={`${css.avatarImg} ${isSearchMob ? "md:block hidden" : ""}`} style={{width: "35px", height:"35px"}} src={User1} alt="user" /></div>
       </div>
     </nav>
   );
